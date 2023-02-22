aa849b8ad022313794850b00e090e4b7e2a96fcd
Rahul <rahulsati9969@gmail.com>
2023-02-22 20:28:03 +1000

    eleven

---
+++ eleven/strings.py
@@ -0,0 +1,41 @@
+name1 = "Harry"
+name2 = "Rohan"
+name3 = "Rahul"
+print(name1, name2, name3)
+
+
+#for these purpose both the quotation mark are allowed
+print('He said "I want to eat an apple"');
+print("Rahul's Birthday")
+
+#we also have triple quote
+a = '''
+hello i am rahul sati
+and i am doing good
+'''
+print(a);
+
+#cannot do this because it will give us error
+# a= "
+# hello
+# rahul
+# "
+
+str = "Rahul sati is a good boy";
+
+print(str[0],str[1], str[2], str[3], str[4], str[5], str[6]);
+#R a h u l   s ->output
+
+
+
+# for loop in strings ek ek krke sare character print kr dega
+for i in str :
+    print(i);
+
+'''
+#!anything written between single or double quotation mark is known as string
+#*string are used to deal with unicode characters
+#*indexing starts from zero in python
+#in python string is like an array of characters
+'''
+
