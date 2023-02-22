c78baa957e650ba48e3960c6a122126923702bed
Rahul <rahulsati9969@gmail.com>
2023-02-22 18:50:24 +1000

    commit

---
+++ fifth/fifth.py
@@ -0,0 +1,32 @@
+# comments are those statements which are ignored by python interpretor
+print("hey i am a good boy")
+
+
+# this is not allowed if you need new line then use escape sequence characters
+# print("aaaaaaaaaaaaaaaaaaaa
+#       ")
+
+
+print("rahul sati is a very good boy \nand he loves coding")
+
+print("my name is rahul sati \"the great boy\"")
+
+# default separator is ' ' and default end is new line
+print("1", "2", "3", "4", "null", sep="->", end=" 009\n")
+
+print("hello rahul")
+
+
+''' 
+these are the multi line line comments in python
+in this way you can write as much as comment you want to write in python
+'''
+
+"""
+this is also the way to write multiline comment
+"""
+
+
+'''
+#!escape sequence characters are used when a character cannot be used in a python programme
+'''
