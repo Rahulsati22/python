92285dd2b69d0e58320f0f297fa6fefa92537490
Rahul <rahulsati9969@gmail.com>
2023-02-22 20:02:07 +1000

    ten

---
+++ ten/input.py
@@ -0,0 +1,22 @@
+a = input('enter the first number\t')
+b = input('enter the second number\t')
+
+# this will give us the output 1020 because by default it is string
+print(a + b)
+
+
+a = int(input('enter the first number\t'))
+b = int(input('enter the second number\t'))
+
+
+# this will give us 30 as we have used typecasting
+print(a+b)
+
+'''
+enter the first number  10
+enter the second number 20
+1020
+enter the first number  10
+enter the second number 20
+30
+'''
---
+++ ten/input2.py
@@ -0,0 +1,3 @@
+a = int(input("enter the first number\t"))
+b = int(input("enter the second number\t"))
+print("the multiplication of the two number is", a * b)
