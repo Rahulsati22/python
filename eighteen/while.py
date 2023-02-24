5ca9a9e64d95763180d6f5250940df47b0377ef2
Rahul <rahulsati9969@gmail.com>
2023-02-24 18:17:04 +1000

    "while loop completed"

---
+++ eighteen/while.py
@@ -0,0 +1,21 @@
+#programme1
+count = 5;
+while (count > 0):
+    print(count,end=' ');
+    count-=1;
+
+print();
+
+
+#programme2
+count = 1;
+while (count <= 5):
+    print(count,end=' ');
+    count+=1;
+
+print();
+
+#programme3
+count = int(input("Enter the number here and enter -1 to exit\n"));
+while (count != -1):
+    count = int(input("Enter the number here and enter -1 to exit\n"));
---
+++ eighteen/while2.py
@@ -0,0 +1,7 @@
+# using while loop with else
+count = 0
+while (count <= 5):
+    print(count)
+    count += 1
+else:
+    print("value of count is greater than 5")
