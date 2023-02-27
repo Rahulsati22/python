e5dccdba92a9780a00a32e36ff6a710f0be812f5
Rahul <rahulsati9969@gmail.com>
2023-02-27 14:44:04 +0530

    :jd

--- thirtyone/sets.py
+++ thirtyone/sets.py
@@ -1,21 +1,21 @@
 '''
 sets are unordered collection of data-items.They store multiple items in a single variable .Set items are separated by commas and enclosed within curly brackets.Sets are unchangeable, meaning you cannot change items of the set once they are created . Sets do not contains duplicate items.
 '''
-s = {2,4,2,6}
-print(s);
-print(type(s));#<class 'set'>
+s = {2, 4, 2, 6}
+print(s)
+print(type(s))  # <class 'set'>
 
 
-s={};
-print(type(s));#<class 'dict'>
+s = {}
+print(type(s))  # <class 'dict'>
 
 
-s=set();
-print(type(s));#<class 'set'>
+s = set()
+print(type(s))  # <class 'set'>
 
 
-s = {"carla", "rahul", 1.2, 1.5, 10,"rahul","rahul"};
-print(s);
+s = {"carla", "rahul", 1.2, 1.5, 10, "rahul", "rahul"}
+print(s)
 
 for i in s:
-    print(i, end=" ");
+    print(i, end=" ")
